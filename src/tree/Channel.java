6f9bb4abc689addf0cc422f2d7f11ec31c0f90e1
LukasBenner <lukasbenner02@gmail.com>
2022-05-26 17:04:30 +0200

    added Channel

--- src/tree/Channel.java
+++ src/tree/Channel.java
@@ -1,6 +1,27 @@
 package tree;
 
 public class Channel extends Node{
+
+    int _sid;
+    String _type;
+    String _name;
+    String _v_pid;
+    String _a_pid;
+    String _compression;
+    String _url;
+    String _enc;
+    String _package;
+    String _res;
+
+    public int getSid() {
+        return _sid;
+    }
+
+    public String getName() {
+        return _name;
+    }
+
+
     @Override
     public Node accept(Object visitor) {
         return null;
