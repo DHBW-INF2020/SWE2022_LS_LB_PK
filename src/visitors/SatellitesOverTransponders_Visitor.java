912096e3504556f7513c9905209b4a5f2095f3e0
Lea Soffel <inf20070@lehre.dhbw-stuttgart.de>
2022-05-26 17:45:46 +0200

    changed Node

--- src/visitors/SatellitesOverTransponders_Visitor.java
+++ src/visitors/SatellitesOverTransponders_Visitor.java
@@ -4,6 +4,7 @@
 package visitors;
 
 import tree.Channel;
+import tree.Root;
 import tree.Satellite;
 import tree.Transponder;
 
@@ -11,24 +12,31 @@ import tree.Transponder;
  * @author Lea
  *
  */
-public class SatellitesOverTransponders_Visitor implements Visitor{
-
+public class SatellitesOverTransponders_Visitor implements IVisitor{
+	
+	private Root _rootNode;
+	
 	@Override
 	public Object visitChannel(Channel ctx) {
-		// TODO Auto-generated method stub
-		return null;
+		return ctx.accept(this);
 	}
 
 	@Override
 	public Object visitSatellite(Satellite ctx) {
-		// TODO Auto-generated method stub
-		return null;
+		return ctx.accept(this);
 	}
 
 	@Override
 	public Object visitTransponder(Transponder ctx) {
-		// TODO Auto-generated method stub
-		return null;
+		return ctx.accept(this);
+	}
+
+	@Override
+	public Object visitRoot(Root ctx) {
+		_rootNode = new Root();
+		
+		
+		return ctx.accept(this);
 	}
 
 }
