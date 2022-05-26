1aa3f607c7ec61a5687af010f3219499c6856352
LukasBenner <lukasbenner02@gmail.com>
2022-05-26 16:13:34 +0200

    added uml class diagram

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,37 @@
+import com.google.gson.*;
+import com.google.gson.stream.JsonReader;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // JsonParser
+        JsonElement json = JsonParser.parseString("""
+        {
+            "number": 1,
+            "object": {
+                "string":"Hallo"
+            },
+            "boolean": true,
+            "array": [
+                47.11,
+                "Hallo nochmal"
+            ]
+        }
+    """);
 
+// JsonReader
+        try (JsonReader reader = new JsonReader(
+                new StringReader(json.toString()))) {
+            reader.beginObject();
+            while(reader.hasNext()) {
+                System.out.println(reader.nextName());
+                reader.skipValue();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ src/tree/Node.java
@@ -0,0 +1,7 @@
+package tree;
+
+import java.util.ArrayList;
+
+
+public abstract class Node {
+}
