7b0c7b9f9507feea677c8d25a418dda36dd1fd14
Lea Soffel <inf20070@lehre.dhbw-stuttgart.de>
2022-05-26 17:30:02 +0200

    added SoT_Visitor

---
+++ src/visitors/SatellitesOverTransponders_Visitor.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package visitors;
+
+import tree.Channel;
+import tree.Satellite;
+import tree.Transponder;
+
+/**
+ * @author Lea
+ *
+ */
+public class SatellitesOverTransponders_Visitor implements Visitor{
+
+	@Override
+	public Object visitChannel(Channel ctx) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitSatellite(Satellite ctx) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object visitTransponder(Transponder ctx) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
