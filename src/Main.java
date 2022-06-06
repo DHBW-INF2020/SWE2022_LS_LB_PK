71a259ef85192db47a99e293f7fcffc0873497d4
LukasBenner <lukasbenner02@gmail.com>
2022-06-06 16:34:05 +0200

    finished json output

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import input.InputHandler;
+import output.JsonOutput_Visitor;
 import tree.Node;
 import aggregates.ChannelsOverSatellites_Visitor;
 import aggregates.SatellitesOverTransponders_Visitor;
@@ -12,13 +16,15 @@ public class Main {
     public static void main(String[] args) {
         Node root = InputHandler.parseJsonToTree("input.json");
         Node newTree = root.accept(new ChannelsOverSatellites_Visitor());
-        newTree = root.accept(new SatellitesOverTransponders_Visitor());
-        Gson gson = new Gson();
-        try{
-            gson.toJson(newTree.getChildren(), new FileWriter("output.json"));
-        }
-        catch (IOException e){
-            throw new RuntimeException(e);
-        }
+        //newTree = root.accept(new SatellitesOverTransponders_Visitor());
+
+        JsonOutput_Visitor jsonOutput_visitor = new JsonOutput_Visitor();
+        newTree.accept(jsonOutput_visitor);
+        String ugly = jsonOutput_visitor.jsonString;
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        JsonElement je = JsonParser.parseString(ugly);
+        String prettyJsonString = gson.toJson(je);
+
+        System.out.println(prettyJsonString);
     }
 }
--- src/output/JsonOutput_Visitor.java
+++ src/output/JsonOutput_Visitor.java
@@ -1,14 +1,86 @@
 package output;
 
-import tree.Root;
+import tree.*;
 import visitor.BaseVisitor;
 
-public class JsonOutput_Visitor extends BaseVisitor {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
-    String jsonString = "";
+public class JsonOutput_Visitor extends BaseVisitor<Node> {
+
+    public String jsonString = "";
 
     @Override
-    public Object visitRoot(Root ctx) {
-        return super.visitRoot(ctx);
+    public Node visitTransponder(Transponder ctx) {
+        jsonString += "{";
+        addAttribute("pol", ctx.getPolarisation(), true);
+        addAttribute("freq", String.valueOf(ctx.getFrequency()), false);
+        addAttribute("sym", ctx.getSymmetry(), false);
+        addChildren(ctx.getChildren(), ctx.getChildrenType());
+        jsonString += "}";
+        return null;
+    }
+
+    @Override
+    public Node visitSatellite(Satellite ctx) {
+        jsonString += "{";
+        addAttribute("sat", ctx.getName(), true);
+        addAttribute("orbital", ctx.getOrbital(), false);
+        addChildren(ctx.getChildren(), ctx.getChildrenType());
+        jsonString += "}";
+
+        return null;
+    }
+
+    @Override
+    public Node visitChannel(Channel ctx) {
+        jsonString += "{";
+        addAttribute("channel", ctx.getName(), true);
+        addChildren(ctx.getChildren(), ctx.getChildrenType());
+        jsonString += "}";
+
+        return null;
+    }
+
+
+    @Override
+    public Node visitRoot(Root ctx) {
+        jsonString += "[";
+        ArrayList<Node> children = ctx.getChildren();
+        for (int i = 0; i < children.size(); i++) {
+            Node child = children.get(i);
+            child.accept(this);
+            if(i < children.size()-1) jsonString += ",";
+
+        }
+        jsonString += "]";
+        return null;
+    }
+
+    private void addChildren(ArrayList<Node> children, NodeType childrenType) {
+        if(children.size() > 0){
+            jsonString += ",";
+            switch (childrenType){
+                case TRANSPONDER -> jsonString+="\"transponders\":";
+                case SATTELITE -> jsonString+="\"satellites\":";
+                case CHANNEL -> jsonString+="\"channels\":";
+            }
+            jsonString += "[";
+            for (int i = 0; i < children.size(); i++) {
+                Node child = children.get(i);
+                child.accept(this);
+                if(i < children.size()-1) jsonString += ",";
+            }
+            jsonString += "]";
+        }
+    }
+
+    private void addAttribute(String name, String value, Boolean first){
+        if(first) {
+            jsonString += "\"" + name + "\": \"" + value + "\"";
+        }
+        else{
+            jsonString += ",\"" + name + "\": \"" + value + "\"";
+        }
     }
 }
